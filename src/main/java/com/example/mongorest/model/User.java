9e006cb9bcb937a7518683551b482aafa4a2081a
veerababu0221 <veerababu0221@gmail.com>
2025-07-09 21:09:46 +0530

    spring version updated to 2.7.14 and DTO validation added

--- src/main/java/com/example/mongorest/model/User.java
+++ src/main/java/com/example/mongorest/model/User.java
@@ -3,6 +3,9 @@ package com.example.mongorest.model;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
+import javax.validation.constraints.Email;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -16,7 +19,13 @@ import lombok.NoArgsConstructor;
 public class User {
     @Id
     private String id;
+    
+    @NotBlank
     private String name;
+
+    @Email
     private String email;
+
+    @Min(18)
     private int age;
 }
